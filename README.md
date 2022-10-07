58033eab2da25209192c243b99a37a6533e47b26
giraldogdiego <diegoagg95@gmail.com>
2022-10-07 18:13:27 -0500

    Release 1.0.0

---
+++ README.md
@@ -0,0 +1,158 @@
+# React Native Tenjin Plugin
+
+# Summary
+
+The Tenjin iOS SDK allows users to track events and installs in their iOS apps. To learn more about Tenjin and our product offering, please visit https://www.tenjin.com.
+
+### Notes:
+
+On iOS:
+- For AppTrackingTransparency, be sure to update your project `.plist` file and add `Privacy - Tracking Usage Description` <a href="https://developer.apple.com/documentation/bundleresources/information_property_list/nsusertrackingusagedescription" target="_new">(NSUserTrackingUsageDescription)</a> along with the text message you want to display to users. This library is only available in iOS 14.0+.
+- For <a href="https://developer.apple.com/documentation/iad/setting_up_apple_search_ads_attribution" target="_new">Apple Search Ads Attribution</a> support, please be sure to upgrade to v1.12.6+ and add the `AdServices.framework` library. This library is only available in iOS 14.3+.
+  
+On Android:
+You will need to add [Google's Install Referrer Library](https://developer.android.com/google/play/installreferrer/library.html) to your gradle dependencies. If you haven’t already installed the [Google Play Services](https://developers.google.com/android/guides/setup) you also need to add it
+```gradle
+dependencies {
+  classpath("com.android.installreferrer:installreferrer:1.1.2")
+  classpath("com.google.android.gms:play-services-analytics:17.0.0")
+}
+```
+
+# Plugin Integration
+
+## Getting started
+`$ npm install react-native-tenjin --save`
+
+### Mostly automatic installation
+`$ react-native link react-native-tenjin`
+
+### Import
+```javascript
+import Tenjin from 'react-native-tenjin';
+```
+
+## Available methods
+
+### Initialize
+You need to initialize the plugin before doing calling any of the other methods available, for this, you would need the api key provided on Tenjin's dashboard:
+```javascript
+Tenjin.initialize(apiKey)
+```
+Parameters:
+- `apiKey`: String
+
+### Connect
+```javascript
+Tenjin.connect()
+```
+
+### Set AppStore type (only available for Android)
+```javascript
+Tenjin.setAppStore(type)
+```
+Parameters:
+- `type`: String, possible values (googleplay, amazon, other)
+
+### OptIn
+```javascript
+Tenjin.optIn()
+```
+
+### OptOut
+```javascript
+Tenjin.optOut()
+```
+
+### OptIn with parameters
+```javascript
+Tenjin.optIn(parameters)
+```
+Parameters:
+- `parameters`: Array
+
+### OptOut with parameters
+```javascript
+Tenjin.optOut(parameters)
+```
+Parameters:
+- `parameters`: Array
+
+### Register transaction
+```javascript
+transaction(productName, currencyCode, quantity, unitPrice)
+```
+Parameters:
+- `productName`: String
+- `currencyCode`: String
+- `quantity`: Number
+- `unitPrice`: Number
+
+### Send event with name
+```javascript
+Tenjin.eventWithName(name)
+```
+Parameters:
+- `name`: String
+
+### Send event with name and value
+```javascript
+Tenjin.eventWithNameAndValue(name, value)
+```
+Parameters:
+- `name`: String
+- `value`: String
+
+### Get attribution info
+```javascript
+Tenjin.getAttributionInfo(
+    (success) => {
+        console.log(`Attibution info found! ${success}`);
+    },
+    () => {
+        console.error(`Attribution info failed`);
+    }
+)
+```
+Parameters:
+- `successCallback`: Callback
+- `errorCallback`: Callback
+
+
+### Send AdMob impression (ILRD)
+```javascript
+Tenjin.eventAdImpressionAdMob(json)
+```
+Parameters:
+- `json`: JSON
+
+### Send AppLovin impression (ILRD)
+```javascript
+Tenjin.eventAdImpressionAppLovin(json)
+```
+Parameters:
+- `json`: JSON
+
+### Send HyperBid impression (ILRD)
+```javascript
+Tenjin.eventAdImpressionHyperBid(json)
+```
+Parameters:
+- `json`: JSON
+
+### Send IronSource impression (ILRD)
+```javascript
+Tenjin.eventAdImpressionIronSource(json)
+```
+Parameters:
+- `json`: JSON
+
+### Send TopOn impression (ILRD)
+```javascript
+Tenjin.eventAdImpressionTopOn(json)
+```
+Parameters:
+- `json`: JSON
+
+## Support
+If you have any issues with the plugin integration or usage, please contact us to support@tenjin.com
---
+++ android/src/main/java/com/reactlibrary/TenjinModule.java
@@ -0,0 +1,214 @@
+// TenjinModule.java
+
+package com.reactlibrary;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.tenjin.android.TenjinSDK;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class TenjinModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+    public TenjinSDK instance = null;
+
+    public TenjinModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "Tenjin";
+    }
+
+    @ReactMethod
+    public void initialize(String apiKey) {
+        instance = TenjinSDK.getInstance(reactContext, apiKey);
+    }
+
+    @ReactMethod
+    public void connect() {
+        instance.connect();
+    }
+
+    @ReactMethod
+    public void setAppStore(String type) {
+        TenjinSDK.AppStoreType appStoreType = TenjinSDK.AppStoreType.googleplay;
+        switch (type) {
+            case "googleplay":
+                appStoreType = TenjinSDK.AppStoreType.googleplay;
+                break;
+            case "amazon":
+                appStoreType = TenjinSDK.AppStoreType.amazon;
+                break;
+            case "other":
+                appStoreType = TenjinSDK.AppStoreType.other;
+                break;
+            default:
+                appStoreType = TenjinSDK.AppStoreType.unspecified;
+                break;
+        }
+        instance.setAppStore(appStoreType);
+    }
+
+    @ReactMethod
+    public void optIn() {
+        instance.optIn();
+    }
+
+    @ReactMethod
+    public void optOut() {
+        instance.optOut();
+    }
+
+    @ReactMethod
+    public void optInParams(ReadableArray params) {
+        instance.optInParams(readableToArray(params));
+    }
+
+    @ReactMethod
+    public void optOutParams(ReadableArray params) {
+        instance.optOutParams(readableToArray(params));
+    }
+
+    @ReactMethod
+    public void transaction(String productName, String currencyCode, Double quantity, Double unitPrice) {
+        instance.transaction(productName, currencyCode, Integer.valueOf(quantity.intValue()), unitPrice);
+    }
+
+    @ReactMethod
+    public void eventWithName(String name) {
+        instance.eventWithName(name);
+    }
+
+    @ReactMethod
+    public void eventWithNameAndValue(String name, String value) {
+        instance.eventWithNameAndValue(name, value);
+    }
+
+    @ReactMethod
+    public void appendAppSubversion(Double version) {
+        instance.appendAppSubversion(Integer.valueOf(version.intValue()));
+    }
+
+    @ReactMethod
+    public void getAttributionInfo(com.facebook.react.bridge.Callback successCallback, com.facebook.react.bridge.Callback errorCallback) {
+        instance.getAttributionInfo(data -> successCallback.invoke(data));
+    }
+
+    @ReactMethod
+    public void eventAdImpressionAdMob(ReadableMap json) {
+        try {
+            instance.eventAdImpressionAdMob(convertMapToJson(json));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ReactMethod
+    public void eventAdImpressionAppLovin(ReadableMap json) {
+        try {
+            instance.eventAdImpressionAppLovin(convertMapToJson(json));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ReactMethod
+    public void eventAdImpressionHyperBid(ReadableMap json) {
+        try {
+            instance.eventAdImpressionHyperBid(convertMapToJson(json));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ReactMethod
+    public void eventAdImpressionIronSource(ReadableMap json) {
+        try {
+            instance.eventAdImpressionIronSource(convertMapToJson(json));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ReactMethod
+    public void eventAdImpressionTopOn(ReadableMap json) {
+        try {
+            instance.eventAdImpressionTopOn(convertMapToJson(json));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String[] readableToArray(ReadableArray readableArray) {
+        String[] array = new String[readableArray.size()];
+        for (int i = 0; i < readableArray.size(); i++) {
+            array[i] = readableArray.getString(i);
+        }
+        return array;
+    }
+
+    private JSONObject convertMapToJson(ReadableMap readableMap) throws JSONException {
+        JSONObject object = new JSONObject();
+        ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+        while (iterator.hasNextKey()) {
+            String key = iterator.nextKey();
+            switch (readableMap.getType(key)) {
+                case Null:
+                    object.put(key, JSONObject.NULL);
+                    break;
+                case Boolean:
+                    object.put(key, readableMap.getBoolean(key));
+                    break;
+                case Number:
+                    object.put(key, readableMap.getDouble(key));
+                    break;
+                case String:
+                    object.put(key, readableMap.getString(key));
+                    break;
+                case Map:
+                    object.put(key, convertMapToJson(readableMap.getMap(key)));
+                    break;
+                case Array:
+                    object.put(key, convertArrayToJson(readableMap.getArray(key)));
+                    break;
+            }
+        }
+        return object;
+    }
+
+    private JSONArray convertArrayToJson(ReadableArray readableArray) throws JSONException {
+        JSONArray array = new JSONArray();
+        for (int i = 0; i < readableArray.size(); i++) {
+            switch (readableArray.getType(i)) {
+                case Null:
+                    break;
+                case Boolean:
+                    array.put(readableArray.getBoolean(i));
+                    break;
+                case Number:
+                    array.put(readableArray.getDouble(i));
+                    break;
+                case String:
+                    array.put(readableArray.getString(i));
+                    break;
+                case Map:
+                    array.put(convertMapToJson(readableArray.getMap(i)));
+                    break;
+                case Array:
+                    array.put(convertArrayToJson(readableArray.getArray(i)));
+                    break;
+            }
+        }
+        return array;
+    }
+}
