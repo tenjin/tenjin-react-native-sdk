d3dcdc28195fb5a3f93af1351c847e97eb3fe879
giraldogdiego <diegoagg95@gmail.com>
2025-01-30 10:50:28 -0500

    Update iOS Cocoapods config

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -2,10 +2,7 @@ const path = require('path');
 const pkg = require('../package.json');
 
 module.exports = {
-  project: {
-    ios: {
-      automaticPodsInstallation: true,
-    },
+  project: { 
     android: {
       packageName: 'tenjin.example',
       sourceDir: './android',
